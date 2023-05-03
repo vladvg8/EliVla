00f1e45360bfda15670bd032e7d3121b507d323f
vetirvex3 <89654370+vladvg8@users.noreply.github.com>
2023-05-03 13:27:38 +0300

    Update phoneForm.js

--- scripts/phoneForm.js
+++ scripts/phoneForm.js
@@ -4,13 +4,13 @@ function getFormPhone() {
     let emailPhone = document.getElementById('emailPhone').value
     let datePhone = document.getElementById('datePhone').value  
     let select = document.getElementById('selectMemberPhone')
+    let formAlert = document.getElementById('submitEvent');
     select.addEventListener('change', function() {
         let getValuePhone = this.value;
     })
     if (fioPhone == '' || telephonePhone == '' || emailPhone == '' || datePhone == '') {
         alert('Уважаемый пользователь, введите все поля.')
     } else {
-        let formAlert = document.getElementById('submitEvent');
         formAlert.className = 'show';
         setTimeout(function () {
         formAlert.className = formAlert.className.replace("show", "")
